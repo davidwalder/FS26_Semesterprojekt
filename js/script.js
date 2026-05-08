eff4e0fdbb97edd919f086226b4e9012001290e4
davidwalder <david.walder@stud.fhgr.ch>
2026-05-08 10:44:56 +0200

    backgroundcolor change

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log("blub");
+async function loadData() {
+    const url1 = 'https://www.vogelwarte.ch/wp-content/assets/json/bird/list_de.json'; // mit korrekter API-URL ersetzen
+    const url2 = 'https://www.vogelwarte.ch/wp-content/assets/json/bird/species/700_de.json'; // mit korrekter API-URL ersetzen
+    try {
+        const response1 = await fetch(url1);
+        return await response1.json();
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}
+const data = await loadData();
+console.log(data); // gibt die Daten der API oder false in der Konsole aus
+
+
